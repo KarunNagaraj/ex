6dc508bc725f8025185791f5c6bde625c16c9ef1
ABCZ234 <amith79058903@gmail.com>
2026-01-08 10:07:31 +0530

    xavier has comitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-   
-   const App: React.FC = () => {
-     return (
-       <div>
-         <h1>Hello Team!</h1>
-       </div>
-     );
-   };
-   
-   export default App;
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello Team!</h1>
+      <h1>xqvier</h1>
+    </div>
+  );
+};
+
+export default App;
